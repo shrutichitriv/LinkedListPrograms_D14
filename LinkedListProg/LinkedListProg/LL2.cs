0ed3889afd57c988cf56925f4c20bc5c47f2335f
shrutichitriv <shrutichitriv199620@gmail.com>
2022-12-11 17:35:29 +0530

    LL2 added

---
+++ LinkedListProg/LinkedListProg/LL2.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LinkedListProg
+{
+    public class Node
+    {
+        public int data;
+        public Node next;
+
+        public Node(int data)
+        {
+            this.data = data;
+        }
+    }
+    public class LinkedList
+    {
+        Node head = null;
+
+        public void Add(int data) //Data Add Into LinkedList
+        {
+            Node node = new Node(data);
+
+            if (this.head == null)
+            {
+                this.head = node;
+                Console.WriteLine(node.data + "Inserted Succesfully");
+
+            }
+            else
+            {
+                Node addr = new Node(data);
+                Node h1 = null;
+                h1 = this.head;
+                this.head = addr;
+                Node temp = addr;
+                temp.next = h1;
+                Console.WriteLine(addr.data + "Inserted Succesfully");
+
+
+            }
+        }
+        public void Display() // Display Accepted Linked List
+        {
+            if (this.head == null)
+            {
+                Console.WriteLine("Empty");
+            }
+            else
+            {
+                Console.WriteLine("------------------------------------------------------");
+                Console.WriteLine("Values In LinkedList Are:");
+                while (this.head != null)
+                {
+                    Console.WriteLine(this.head.data + " ");
+                    this.head = this.head.next;
+                }
+            }
+        }
+    }
+}
--- LinkedListProg/LinkedListProg/Program.cs
+++ LinkedListProg/LinkedListProg/Program.cs
@@ -7,11 +7,18 @@ namespace LinkedListProg
         static void Main(string[] args)
         {
             //LL1
-            LinkedList list=new LinkedList();
-            list.add(56);
-            list.add(30);
-            list.add(70);
-            list.Display(); 
+            //LinkedList list=new LinkedList();
+            //list.add(56);
+            //list.add(30);
+            //list.add(70);
+            //list.Display(); 
+
+            //LL2
+            LinkedList L=new LinkedList();
+            L.Add(70);//Add Data Into LinkedList
+            L.Add(30);
+            L.Add(56);
+            L.Display();//Display Function Call
         }
     }
 }
