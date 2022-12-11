78dffa57dc6cc0f69e4c603155708de9072f8253
shrutichitriv <shrutichitriv199620@gmail.com>
2022-12-11 17:57:02 +0530

    LL4 added

--- LinkedListProg/LinkedListProg/Program.cs
+++ LinkedListProg/LinkedListProg/Program.cs
@@ -21,12 +21,19 @@ namespace LinkedListProg
             //L.Display();//Display Function Call
 
             //LL3
-            LinkedList l = new LinkedList();//create linkedlist object
-            l.add(56);//add data into list
-            l.add(30);
-            l.add(70);
-            l.Display();//call display
+            //LinkedList l = new LinkedList();//create linkedlist object
+            //l.add(56);//add data into list
+            //l.add(30);
+            //l.add(70);
+            //l.Display();//call display
 
+            //LL4
+            LinkedList l = new LinkedList();//Create Linked list Object
+            l.Add(56);//Add Linked list Data
+            l.Add(70);
+            l.Display();
+            l.insertbetween(56, 30);//Call Insert Between function
+            l.Display();
         }
     }
 }
